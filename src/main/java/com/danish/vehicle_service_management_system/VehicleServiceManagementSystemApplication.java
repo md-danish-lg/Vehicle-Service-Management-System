cc00be0e6e6c150d9a8fce82e52f1ec408e674d9
md-danish-lg <dk665226@gmail.com>
2026-07-05 11:13:51 +0500

    starter

---
+++ src/main/java/com/danish/vehicle_service_management_system/VehicleServiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.danish.vehicle_service_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleServiceManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danish/vehicle_service_management_system/VehicleServiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danish.vehicle_service_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
