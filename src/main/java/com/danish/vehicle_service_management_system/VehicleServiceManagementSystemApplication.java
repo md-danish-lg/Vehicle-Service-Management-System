4e6f92c218a2f2e28f0823f9fcc26c5003c796cc
md-danish-lg <dk665226@gmail.com>
2026-07-14 14:14:15 +0500

    add cors configuration

--- src/main/java/com/danish/vehicle_service_management_system/VehicleServiceManagementSystemApplication.java
+++ src/main/java/com/danish/vehicle_service_management_system/VehicleServiceManagementSystemApplication.java
@@ -1,10 +1,14 @@
 package com.danish.vehicle_service_management_system;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
@@ -21,6 +25,16 @@ public class VehicleServiceManagementSystemApplication {
         return "STATUS: WORKING";
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+            }
+        };
+    }
 
 }
